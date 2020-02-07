f654a02e8693c6ce5f13ab6a1bcb28f2e87313fe
Sandor Desi <d.alex.galaxy@gmail.com>
2020-02-07 11:47:06 +0000

    Updates

---
+++ precache-manifest.af7427604e5bbfe1fc952667d15c9ede.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "accc8e66a4ea90e5581ad3cf1fb0d6a6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a9ae0139d288aae3b99c",
+    "url": "/monsters-rolodex/static/css/main.b09af7c2.chunk.css"
+  },
+  {
+    "revision": "5972ab9a24d87de2b217",
+    "url": "/monsters-rolodex/static/js/2.40aee04e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.40aee04e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9ae0139d288aae3b99c",
+    "url": "/monsters-rolodex/static/js/main.20f37964.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
